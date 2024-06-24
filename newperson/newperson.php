b7ae7ca793103937533070871de140bc7e3af334
Jean Paul <jeanpaul.puerta@utp.edu.co>
2024-06-24 14:53:20 -0500

    empty

--- newperson/newperson.php
+++ newperson/newperson.php
@@ -27,14 +27,14 @@
             <!-- Cédula -->
             <div class="mb-4">
                 <label for="Cedula" class="block text-sm font-medium text-gray-700">Cédula:</label>
-                <input type="text" id="Cedula" name="Cedula" 
+                <input type="number" id="Cedula" name="Cedula" 
                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
             </div>
 
             <!-- Teléfono -->
             <div class="mb-4">
                 <label for="Telefono" class="block text-sm font-medium text-gray-700">Teléfono:</label>
-                <input type="tel" id="Telefono" name="Telefono"
+                <input type="number" id="Telefono" name="Telefono"
                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
             </div>
 
