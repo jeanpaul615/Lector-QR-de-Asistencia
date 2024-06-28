22b2df5c2a3c024ac711a0b1bc0fb48bc284530a
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-06-28 12:29:32 -0500

    deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'miempresa/mi-paquete',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '795fc1aedf165ea0be40033a86f61601353e5d05',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'miempresa/mi-paquete' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '795fc1aedf165ea0be40033a86f61601353e5d05',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
