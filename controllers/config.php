06ae151a13297abe7c18c0fea2d27f14d1d21863
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-06-28 14:07:03 -0500

    change final

--- controllers/config.php
+++ controllers/config.php
@@ -1,10 +1,10 @@
 <?php
 include "cors.php";
 
-$servidor = 'ewr1.clusters.zeabur.com';
+$servidor = 'viaduct.proxy.rlwy.net';
 $usuario = 'root';
-$contrasena = 'dvtOH2N6Y0pLjqyW1E9k73A8T45hCJzx';   
-$nombre_de_base = 'lectorqr';
+$contrasena = 'NGNVjkRLcvEdnDDXjugNgvAvsXbpRrZw';   
+$nombre_de_base = 'railway';
 
 try {
    $base_de_datos = new PDO("mysql:host=$servidor;dbname=$nombre_de_base", $usuario, $contrasena);
