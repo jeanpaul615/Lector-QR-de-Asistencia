a959871cd615d04da1dde3e4c04f27ef306aec53
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-06-28 13:56:56 -0500

    change config

--- controllers/config.php
+++ controllers/config.php
@@ -1,13 +1,28 @@
 <?php
 include "cors.php";
+
 $servidor = 'ewr1.clusters.zeabur.com';
 $usuario = 'root';
 $contrasena = 'dvtOH2N6Y0pLjqyW1E9k73A8T45hCJzx';   
-$nombre_de_base = 'zeabur';
+$nombre_de_base = 'lectorqr';
 
-try{
+try {
    $base_de_datos = new PDO("mysql:host=$servidor;dbname=$nombre_de_base", $usuario, $contrasena);
-}catch(Exception $e){
+
+   // Configura el modo de errores de PDO para lanzar excepciones
+   $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   // Opcional: configurar el conjunto de caracteres
+   $base_de_datos->exec("SET NAMES 'utf8'");
+
+   // Ejemplo de consulta
+   $query = "SELECT * FROM tu_tabla";
+   $statement = $base_de_datos->query($query);
+   $resultado = $statement->fetchAll(PDO::FETCH_ASSOC);
+   
+   // Puedes usar $resultado para manipular datos
+   
+} catch(Exception $e) {
    echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
    exit;
 }
