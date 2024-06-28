f026eddf01247dfb0eadc6f0ef84c1738567efe9
jeanpaul615 <jeanpaul.puerta@utp.edu.co>
2024-06-28 15:17:18 -0500

    finalmente

--- controllers/config.php
+++ controllers/config.php
@@ -11,17 +11,6 @@ try {
 
    // Configura el modo de errores de PDO para lanzar excepciones
    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-   // Opcional: configurar el conjunto de caracteres
-   $base_de_datos->exec("SET NAMES 'utf8'");
-
-   // Ejemplo de consulta
-   $query = "SELECT * FROM tu_tabla";
-   $statement = $base_de_datos->query($query);
-   $resultado = $statement->fetchAll(PDO::FETCH_ASSOC);
-   
-   // Puedes usar $resultado para manipular datos
-   
 } catch(Exception $e) {
    echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
    exit;
